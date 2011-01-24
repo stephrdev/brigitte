d3f44128d81d86674912e9c36ef8eeca730b27f3
Stephan Jaekel <steph@rdev.info>
2011-01-24 21:28:21 +0100

    make nice breadcrumbs,

--- brigitte/repositories/utils.py
+++ brigitte/repositories/utils.py
@@ -28,3 +28,19 @@ def pygmentize(mime, blob):
 
     return mark_safe(pygmented_string)
 
+def build_path_breadcrumb(path):
+    if not path:
+        return []
+    links = []
+    cur_path = None
+    for part in path.split('/'):
+        if cur_path:
+            cur_path += '/' +part
+        else:
+            cur_path = part
+        links.append({
+            'path': cur_path,
+            'name': part,
+        })
+    return links
+
