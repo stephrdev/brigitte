583f5c898ad4aa690d9ef04a0946fb4483fe82e6
Stephan Jaekel <steph@rdev.info>
2011-01-28 21:07:09 +0100

    url shortcut for repository urls.

--- brigitte/repositories/templatetags/repositories_tags.py
+++ brigitte/repositories/templatetags/repositories_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from brigitte.repositories.utils import pygmentize
 
+from django.template.defaulttags import url
+
 register = template.Library()
 
 @register.filter
@@ -10,4 +12,23 @@ def pygmentize_diff(blob):
     except:
         return blob
 
+@register.tag(name='repo_url')
+def do_repo_url(parser, token):
+    func_name, view_name, token_parts = token.contents.split(' ', 2)
+
+    token_parts = token_parts.split(' ', 1)
+    if len(token_parts) > 1:
+        repo_obj_name = token_parts[0]
+        new_token = token_parts[1]
+    else:
+        repo_obj_name = token_parts[0]
+        new_token = ''
+
+    return url(parser, template.Token(token.token_type, '%s %s %s.user.username %s.slug %s' % (
+        func_name,
+        view_name,
+        repo_obj_name,
+        repo_obj_name,
+        new_token
+    )))
 
