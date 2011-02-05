4ee2a8467be069a03a407d1fa9f7aef6a00bdaf3
Stephan Jaekel <steph@rdev.info>
2011-02-05 22:35:37 +0100

    some gitolite methods.

--- brigitte/repositories/management/commands/export_gitolite.py
+++ brigitte/repositories/management/commands/export_gitolite.py
@@ -1,27 +1,21 @@
 from django.core.management.base import BaseCommand
+from django.conf import settings
 
-from brigitte.repositories.models import Repository
+from brigitte.repositories.gitolite import generate_gitolite_conf, export_public_keys
+
+import os
+
+BRIGITTE_GIT_ADMIN_PATH = getattr(settings,
+                                  'BRIGITTE_GIT_ADMIN_PATH',
+                                  'gitolite-admin')
 
 class Command(BaseCommand):
     help = 'Export gitolite config'
 
     def handle(self, *args, **options):
-        debug = False
-        def generate_access_rule(repo_user, key):
-            if repo_user.can_write and key.can_write:
-                return 'RW+'
-            elif repo_user.can_read and key.can_read:
-                return 'R'
 
-        for repo in Repository.objects.all():
-            if debug:
-                print '# repository: %s' % repo
-            print 'repo\t%s' % repo.short_path
-            for user in repo.repositoryuser_set.all():
-                if debug:
-                    print '\t# user: %s' % user
-                    print '\t# read: %s, write: %s' % (user.can_read, user.can_write)
-                for key in user.user.sshpublickey_set.all():
-                    if debug:
-                        print '\t# key - read: %s, write: %s' % (key.can_read, key.can_write)
-                    print '\t%s\t= key-%s' % (generate_access_rule(user, key), key.pk)
+        generate_gitolite_conf(os.path.join(BRIGITTE_GIT_ADMIN_PATH,
+                                            'conf/gitolite.conf'))
+
+        export_public_keys(os.path.join(BRIGITTE_GIT_ADMIN_PATH, 'keydir'))
+
