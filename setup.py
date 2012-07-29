c856838194cdf2bdac0c1c74577ee00aa9780ac8
Stephan Jaekel <steph@rdev.info>
2012-07-29 11:14:53 +0200

    Updated settings and requirements.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name='brigitte',
     version='0.2',
-    packages=find_packages(),
+    packages=find_packages(exclude=['brigitte_site', 'brigitte_site.*']),
     zip_safe=False,
 )
