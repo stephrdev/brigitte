f6f38a241c38cb70947de05f8da7bd6fc3617845
Stephan Jaekel <steph@rdev.info>
2013-01-13 16:47:23 +0100

    Updated docs and sample config.

--- brigitte_site/dev.sample.py
+++ brigitte_site/dev.sample.py
@@ -2,39 +2,14 @@ from global_settings import *
 
 DEBUG = TEMPLATE_DEBUG = THUMBNAIL_DEBUG = True
 
-# database configuration
+SECRET_KEY = ''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'local_database.db'
     }
 }
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': '',
-#        'USER': '',
-#        'PASSWORD': '',
-#        'OPTIONS': {
-#            'autocommit': True,
-#        }
-#    }
-#}
-
-
-ADMINS = (
-    ('root', 'root@localhost'),
-)
-MANAGERS = ADMINS
-
-# default mail settings
-DEFAULT_FROM_EMAIL = 'root@localhost'
-SERVER_EMAIL = 'root@localhost'
-
-BRIGITTE_GIT_BASE_PATH = os.path.join(PROJECT_ROOT, '..', 'brigitte_repos')
-BRIGITTE_SSH_PORT = 2222
-BRIGITTE_SSH_KEY_PATH = os.path.join(PROJECT_ROOT, 'sshserver_key')
 
 CACHES = {
     'default': {
@@ -45,3 +20,17 @@ CACHES = {
         },
     },
 }
+
+ADMINS = (
+    ('root', 'root@localhost'),
+)
+MANAGERS = ADMINS
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '..', '..', 'static_media')
+
+DEFAULT_FROM_EMAIL = 'root@localhost'
+SERVER_EMAIL = 'root@localhost'
+
+BRIGITTE_GIT_BASE_PATH = os.path.join(PROJECT_ROOT, '..', '..', 'repositories')
+BRIGITTE_SSH_KEY_PATH = os.path.join(PROJECT_ROOT, '..', '..', 'sshserver_key')
+BRIGITTE_SSH_PORT = 2222
